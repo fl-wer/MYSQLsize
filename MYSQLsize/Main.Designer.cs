88915047eb3cf63df924f0eb3fa195c884737a0c
fl-wer <flwer9650@gmail.com>
2022-07-23 19:58:48 +0100

    Disabled wrong option, maximize window is now disabled while minimize is re-enabled.

--- MYSQLsize/Main.Designer.cs
+++ MYSQLsize/Main.Designer.cs
@@ -667,7 +667,7 @@
             this.Controls.Add(this.TINYINTnum);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimizeBox = false;
+            this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "MYSQL Size Calculator";
             this.Load += new System.EventHandler(this.Main_Load);
